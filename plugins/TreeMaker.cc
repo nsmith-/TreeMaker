b10f3333469b5799584445a16b19d4720001716c
Nick Smith <ncsmith6@wisc.edu>
2015-08-11 16:28:27 +0200

    Simple tree with std::vector

--- plugins/TreeMaker.cc
+++ plugins/TreeMaker.cc
@@ -9,7 +9,14 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "DataFormats/Candidate/interface/Candidate.h"
+
+#include "CommonTools/Utils/interface/StringObjectFunction.h"
 
 #include "TTree.h"
 
@@ -25,29 +32,34 @@ class TreeMaker : public edm::one::EDAnalyzer<edm::one::SharedResources> {
     virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
     virtual void endJob() override;
 
-    std::unique_ptr<TTree> tree_;
+    void dumpPSet(const edm::ParameterSet&);
+
+    edm::EDGetTokenT<reco::CandidateView> token_;
+    StringObjectFunction<reco::Candidate, true> function_;
+
+    // Owned by TFileService
+    TTree * tree_;
+
+    int run_;
+    int lumi_;
+    int event_;
+    std::vector<float> vector_;
 };
 
-TreeMaker::TreeMaker(const edm::ParameterSet& iConfig)
+TreeMaker::TreeMaker(const edm::ParameterSet& iConfig) :
+  token_(consumes<reco::CandidateView>(iConfig.getParameter<edm::InputTag>("candidates"))),
+  function_(iConfig.getParameter<std::string>("function"))
 {
   usesResource("TFileService");
-
-  edm::LogPrint("TreeMaker") << "Parameter names:" << std::endl << "------------------------------";
-  std::function<void(const edm::ParameterSet&, int)> dumpPSetNames = [&dumpPSetNames](const edm::ParameterSet& pset, int indent) {
-    std::string spaces{};
-    spaces.resize(indent*2, ' ');
-    for (auto name : pset.getParameterNames()) {
-      edm::LogPrint("TreeMaker") << spaces << name;
-      if ( pset.existsAs<edm::ParameterSet>(name) ) {
-        dumpPSetNames(pset.getParameterSet(name), indent+1);
-      }
-    }
-  };
-  dumpPSetNames(iConfig, 0);
-
-  std::string treeName{iConfig.getParameter<std::string>("@module_label")};
+  std::string treeName{iConfig.getUntrackedParameter<std::string>("treeName", "Ntuple")};
   std::string treeTitle{treeName};
-  tree_ = std::unique_ptr<TTree>{new TTree(treeName.c_str(), treeTitle.c_str())};
+
+  edm::Service<TFileService> fs;
+  tree_ = fs->make<TTree>(treeName.c_str(), treeTitle.c_str());
+  tree_->Branch("run", &run_, "run/i");
+  tree_->Branch("lumi", &lumi_, "lumi/i");
+  tree_->Branch("event", &event_, "event/l");
+  tree_->Branch("vect", "std::vector<float>", &vector_);
 }
 
 
@@ -58,6 +70,19 @@ TreeMaker::~TreeMaker()
 void
 TreeMaker::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
+  run_ = iEvent.id().run();
+  lumi_ = iEvent.id().luminosityBlock();
+  event_ = iEvent.id().event();
+
+  edm::Handle<reco::CandidateView> candidates;
+  iEvent.getByToken(token_, candidates);
+
+  vector_.clear();
+  for ( const auto& candidate : *candidates ) {
+    vector_.push_back(function_(candidate));
+  }
+
+  tree_->Fill();
 }
 
 
@@ -78,5 +103,22 @@ TreeMaker::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
   desc.setUnknown();
   descriptions.addDefault(desc);
 }
+
+void
+TreeMaker::dumpPSet(const edm::ParameterSet& iConfig) {
+  edm::LogPrint("TreeMaker") << "Parameter names:" << std::endl << "------------------------------";
+  std::function<void(const edm::ParameterSet&, int)> dumpPSetNames = [&dumpPSetNames](const edm::ParameterSet& pset, int indent) {
+    std::string spaces{};
+    spaces.resize(indent*2, ' ');
+    for (auto name : pset.getParameterNames()) {
+      edm::LogPrint("TreeMaker") << spaces << name;
+      if ( pset.existsAs<edm::ParameterSet>(name) ) {
+        dumpPSetNames(pset.getParameterSet(name), indent+1);
+      }
+    }
+  };
+  dumpPSetNames(iConfig, 0);
+}
+
 
 DEFINE_FWK_MODULE(TreeMaker);
--- test/testConfig.py
+++ test/testConfig.py
@@ -6,14 +6,25 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
 
-process.source = cms.Source("EmptySource")
-
-process.demo = cms.EDAnalyzer('TreeMaker',
-    asdf = cms.PSet(
-        hi = cms.bool(True),
-        basdf = cms.string("hi")
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+        '/store/data/Run2015B/ZeroBias/MINIAOD/PromptReco-v1/000/251/244/00000/4CE7A263-0728-E511-818D-02163E01379D.root'
     ),
 )
 
+process.TFileService = cms.Service(
+    "TFileService",
+    fileName = cms.string("testConfig.root")
+)
 
-process.p = cms.Path(process.demo)
+process.met = cms.EDAnalyzer('TreeMaker',
+    candidates = cms.InputTag("slimmedMETs"),
+    function = cms.string("pt-uncorrectedPt"),
+)
+
+process.jet = cms.EDAnalyzer('TreeMaker',
+    candidates = cms.InputTag("slimmedJets"),
+    function = cms.string("pt"),
+)
+
+process.p = cms.Path(process.met+process.jet)
