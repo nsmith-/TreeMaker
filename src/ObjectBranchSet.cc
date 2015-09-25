9beda75fcc75d5654e17fd2c1120f53dc2415d5c
Nick Smith <nick.smith@cern.ch>
2015-09-25 12:11:50 +0200

    Generic Object branch set
    Closes #1
    Note: getByLabel is not being deprecated, just need to declare the consumes.
    See https://hypernews.cern.ch/HyperNews/CMS/get/edmFramework/3565/1/1.html

--- src/ObjectBranchSet.cc
+++ src/ObjectBranchSet.cc
@@ -9,6 +9,13 @@
 ObjectBranchSet::ObjectBranchSet(TTree * tree, std::string collectionName, const edm::ParameterSet& iConfig, edm::ConsumesCollector cc) {
   typeName_ = iConfig.getParameter<std::string>("productType");
   inputTag_ = iConfig.getParameter<edm::InputTag>("inputTag");
+
+  // Although we cannot use getByToken with GenericHandle,
+  // we still need to declare the consumption
+  auto typeInfo = edm::TypeID(edm::TypeWithDict::byName(typeName_).typeInfo());
+  auto consumesType = edm::TypeToGet(typeInfo, edm::PRODUCT_TYPE);
+  cc.consumes(consumesType, inputTag_);
+
   for ( auto functionName : iConfig.getParameterSet("functions").getParameterNamesForType<std::string>() ) {
     auto functionString = iConfig.getParameterSet("functions").getParameter<std::string>(functionName);
     branches_.emplace_back(new StringObjectFunctionBranch(tree, collectionName+"_"+functionName, functionString, typeName_));
